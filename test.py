751a9fba518efaab9cd9ea54233e6974b137cdb0
dv <wdv4758h@gmail.com>
2014-10-20 01:21:59 +0800

    [triangle] add more equivalence class test

--- test.py
+++ test.py
@@ -3,16 +3,26 @@ import hw1
 
 class TestHw1(unittest.TestCase):
     def test_triangle(self):
-        cases = [((5, 5, 5), 'Equilateral'),
-                 ((2, 2, 3), 'Isosceles'),
-                 ((3, 4, 5), 'Scalene'),
-                 ((4, 1, 2), 'Not a triangle'),
-                 ((-1, 5, 5), 'Value of a is not in the range of permitted values'),
-                 ((5, -1, 5), 'Value of b is not in the range of permitted values'),
-                 ((5, 5, -1), 'Value of c is not in the range of permitted values'),
+        cases = [
+                 # equivalence class
+                 ((5, 5, 5),   'Equilateral'),
+                 ((2, 2, 3),   'Isosceles'),
+                 ((3, 4, 5),   'Scalene'),
+                 ((4, 1, 2),   'Not a triangle'),
+                 ((-1, 5, 5),  'Value of a is not in the range of permitted values'),
+                 ((5, -1, 5),  'Value of b is not in the range of permitted values'),
+                 ((5, 5, -1),  'Value of c is not in the range of permitted values'),
                  ((201, 5, 5), 'Value of a is not in the range of permitted values'),
                  ((5, 201, 5), 'Value of b is not in the range of permitted values'),
-                 ((5, 5, 201), 'Value of c is not in the range of permitted values')]
+                 ((5, 5, 201), 'Value of c is not in the range of permitted values'),
+                 ((-1, 5, 5),  'Value of a is not in the range of permitted values'),
+                 ((5, -1, 5),  'Value of b is not in the range of permitted values'),
+                 ((5, 5, -1),  'Value of c is not in the range of permitted values'),
+                 ((-1, -1, 5), 'Value of a,b is not in the range of permitted values'),
+                 ((5, -1, -1), 'Value of b,c is not in the range of permitted values'),
+                 ((-1, 5, -1), 'Value of a,c is not in the range of permitted values'),
+                 ((-1, -1, -1),'Value of a,b,c is not in the range of permitted values'),
+        ]
 
         for value, result in cases:
             self.assertEqual(hw1.triangle(*value), result)
