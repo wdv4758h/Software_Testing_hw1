35a4d0cf8b9efc7fe1552846312731d9f3c5f778
dv <wdv4758h@gmail.com>
2014-10-20 01:21:00 +0800

    [triangle] more robust boundary detect

--- hw1.py
+++ hw1.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 def triangle(a, b, c):
+    out_of_bound = ""
     if a < 0 or a > 200:
-        return 'Value of a is not in the range of permitted values'
+        out_of_bound += "a"
     if b < 0 or b > 200:
-        return 'Value of b is not in the range of permitted values'
+        out_of_bound += "b"
     if c < 0 or c > 200:
-        return 'Value of c is not in the range of permitted values'
+        out_of_bound += "c"
+
+    if out_of_bound:
+        return 'Value of {} is not in the range of permitted values'.format(','.join(out_of_bound))
 
     edge = sorted([a, b, c])
 
